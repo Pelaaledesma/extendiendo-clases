7a41bc10c393e5c7da37b7098f5a7187a45d8bfb
Cristian Matias Ledesma <cristianmatiasledesma@gmail.com>
2025-07-15 12:46:27 -0300

    Test Pass!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import products from "./products.json";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +25,44 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    super(n);
+    products.forEach((producto) => {
+      this.addProduct({
+        id: producto.id,
+        name: producto.name,
+        price: producto.price,
+      });
+    });
+  }
+  addProduct(product: Product): void {
+    const exists = this.cosas.some((p) => p.id === product.id);
+    if (!exists) {
+      this.cosas.push(product);
+    }
+  }
+  getProduct(id: number): Product {
+    return this.cosas.find((p) => p.id === id);
+  }
+  removeProduct(id: number): void {
+    this.cosas = this.cosas.filter((p) => p.id !== id);
+  }
+  getSortedByPrice(order: string): Product[] {
+    if (order === "asc") {
+      this.cosas.sort((a, b) => a.price - b.price);
+      return this.cosas;
+    }
+    if (order === "desc") {
+      this.cosas.sort((a, b) => b.price - a.price);
+      return this.cosas;
+    } else {
+      console.log(
+        'Los parametros a ingresar deberian ser "asc" para ascendente y "desc" para descendente.'
+      );
+      return [];
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
